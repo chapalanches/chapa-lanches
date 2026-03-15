1151a1d9985ccc93c2187b2bf81e279dd9210881
chapalanches <chapa.lanches235@gmail.com>
2026-03-15 00:52:19 -0300

    Create config.js

---
+++ js/config.js
@@ -0,0 +1,6 @@
+const APP_CONFIG = {
+  supabaseUrl: "https://ilshtsqcxkiuldatazzo.supabase.co",
+  supabaseAnonKey: "sb_publishable_xKmzl2nMgJVSyD4Ys5KoZg_xpGwiR3h",
+  whatsappNumber: "5515996179172",
+  storeName: "Chapa Lanches"
+};
