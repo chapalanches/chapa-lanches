a984f84c5ff83018ca76018bd23ec960d30eab48
chapalanches <chapa.lanches235@gmail.com>
2026-03-15 18:08:51 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window.APP_CONFIG = {
   supabaseUrl: "https://ilshtsqcxkiuldatazzo.supabase.co",
   supabaseAnonKey: "sb_publishable_xKmzl2nMgJVSyD4Ys5KoZg_xpGwiR3h",
-  googleMapsApiKey: "AIzaSyBjN6AhBnEqgszIRUnEAkk6Gl7Kze18mvs",
+  googleMapsApiKey: "AIzaSyBLA7ZbAj4y0fQYY4kbfi_mcZAmw-LcZmg",
   whatsappNumber: "5515996179172",
   storeName: "Chapa Lanches"
 };
